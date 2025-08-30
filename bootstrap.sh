85beef43d7308cca4e733fc0b3d411d0815b585e
Dmytro <demon9709@gmail.com>
2025-08-30 18:57:21 +0300

    Solution1

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@ kind create cluster --config cluster.yml
 
 kubectl apply -f .infrastructure/namespace.yml
 kubectl apply -f .infrastructure/secret.yml
-kubectl apply -f .infrastructure/configMap-mysql.yml
-kubectl apply -f .infrastructure/clusterIp-mysql.yml
+kubectl apply -f .infrastructure/configMap.yml
+kubectl apply -f .infrastructure/clusterIp.yml
 kubectl apply -f .infrastructure/statefulSet.yml
 kubectl apply -f .infrastructure/app-db-secret.yml
 kubectl apply -f .infrastructure/deployment.yml
