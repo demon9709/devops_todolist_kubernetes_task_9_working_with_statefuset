df7a38f7cdce07b7701bef71e93845f67a899a07
Dmytro <demon9709@gmail.com>
2025-08-30 19:11:24 +0300

    soluttion

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,7 @@ kubectl apply -f .infrastructure/secret.yml
 kubectl apply -f .infrastructure/configMap.yml
 kubectl apply -f .infrastructure/clusterIp.yml
 kubectl apply -f .infrastructure/statefulSet.yml
+
+kubectl apply -f .infrastructure/namespace-todoapp.yml
 kubectl apply -f .infrastructure/app-db-secret.yml
 kubectl apply -f .infrastructure/deployment.yml
